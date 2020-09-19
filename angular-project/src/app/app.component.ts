57b1dcd409f1ea39d3746f8cb47af3884a54110c
msraosurya <ptlmnj89@gmail.com>
2020-09-19 17:06:23 +0530

    app html css

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
 @Component({
   // meta data
   selector: 'app-start',
-  template: '<h1>Hello World..!</h1>',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class FirstComponent {}
--- angular-project/src/main.ts
+++ angular-project/src/main.ts
@@ -4,3 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { FirstModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(FirstModule);
+
+/*
+
+1. index.   -> 2. main.ts  -> 3. app.module.ts -> 4. app.comp.ts / html /css 
+*/
